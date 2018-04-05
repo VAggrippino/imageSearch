a0e87e5c19f502be097947829831acb8185163b5
Vince Aggrippino <vinceaggrippino@gmail.com>
2018-04-05 21:18:23 +0800

    Complete implementation of search and insert query

--- index.js
+++ index.js
@@ -3,8 +3,24 @@ const express = require('express')
 const https = require('https')
 const MongoClient = require('mongodb').MongoClient
 
+// App will listen on this port number
 const port = process.env.PORT || 3000
 
+// Database connection information from .env file
+const database = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  name: process.env.DB_NAME,
+  collection: process.env.DB_COLLECTION,
+  user: process.env.DB_USER,
+  pass: process.env.DB_PASS,
+}
+database['url'] = `mongodb://${database.host}:${database.port}/${database.name}`
+
+// Create a collection to store the searches
+createCollection(database)
+
+// Set up the express app
 const app = express()
 app.set('view engine', 'pug')
 app.use(express.static('public')) // For static CSS / JavaScript files
@@ -15,6 +31,8 @@ app.get('/', (req, res) => {
 
 app.get('/api/imagesearch/:query', (req, res) => {
   let query = req.params.query
+
+  insertSearch(database, query)
 
   let cseParams = '' +
     `?q=${query}` +
@@ -50,7 +68,6 @@ app.get('/api/imagesearch/:query', (req, res) => {
         searchResults.push(resultItem)
       })
       res.end(JSON.stringify(searchResults))
-      // res.end(jsonString)
     })
   })
 
@@ -64,3 +81,39 @@ app.get('/api/imagesearch/:query', (req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port} ...`)
 })
+
+async function dbClient (database) {
+  return MongoClient.connect(database.url)
+}
+
+async function createCollection (database) {
+  console.log(`Creating collection: ${database.collection}`)
+  let client = await dbClient(database)
+  let db = client.db(database.name)
+  let collection = await db.createCollection(database.collection)
+  client.close()
+  return collection
+}
+
+async function insertSearch (database, query) {
+  let client = await dbClient(database)
+  let db = client.db(database.name)
+  let collection = db.collection(database.collection)
+
+  let lastInserted = await collection.findOne({}, {sort: {_id: -1}})
+  if (lastInserted === null || lastInserted.query !== query) {
+    console.log(`Inserting record for new search: ${query}`)
+    collection.insertOne({query})
+  } else {
+    console.log(`Not inserting record for repeated search: ${query}`)
+  }
+}
+
+async function getSearches (database) {
+  let client = await dbClient(database)
+  let db = client.db(database.name)
+  let collection = db.collection(database.collection)
+
+  let results = await collection.find({}, {projection: {_id: 0}})
+  return results
+}

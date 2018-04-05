008ddfaf1c57f64256def68f74e772b1558c8aeb
Vince Aggrippino <vinceaggrippino@gmail.com>
2018-04-05 12:01:53 +0800

    Install pug, set up view engine

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ let express = require('express')
 let MongoClient = require('mongodb').MongoClient
 
 let app = express()
+app.set('view engine', 'pug')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})

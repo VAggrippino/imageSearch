cda60ac3725360b52a2e644472d0728db19121c9
Vince Aggrippino <vinceaggrippino@gmail.com>
2018-04-05 12:09:13 +0800

    Create initial view and start listening

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ require('dotenv').config()
 let express = require('express')
 let MongoClient = require('mongodb').MongoClient
 
+let port = process.env.PORT || 3000
+
 let app = express()
 app.set('view engine', 'pug')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port} ...`)
 })

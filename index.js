30ca7d838df1e294144535dcd52c127c166fab52
Vince Aggrippino <vinceaggrippino@gmail.com>
2018-04-05 20:09:24 +0800

    Remove google-images and access CSE API directly

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 require('dotenv').config()
 const express = require('express')
+const https = require('https')
 const MongoClient = require('mongodb').MongoClient
-const GoogleImages = require('google-images')
 
 const port = process.env.PORT || 3000
-const gimg = new GoogleImages(process.env.CSE_ID, process.env.API_KEY)
 
 const app = express()
 app.set('view engine', 'pug')
@@ -15,8 +14,51 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/imagesearch/:query', (req, res) => {
-  console.log(`Search Query: ${req.params.query}`)
-  res.end('JSON response here')
+  let query = req.params.query
+
+  let cseParams = '' +
+    `?q=${query}` +
+    `&cx=${process.env.CSE_ID}` +
+    `&key=${process.env.API_KEY}` +
+    '&num=10' +
+    '&safe=high' +
+    '&searchType=image' +
+    `&start=${req.query.offset || 1}`
+
+  let options = {
+    hostname: 'www.googleapis.com',
+    path: '/customsearch/v1' + encodeURI(cseParams)
+  }
+
+  console.log(`Search Query: ${query}`)
+  let cseRequest = https.request(options, cseResponse => {
+    let jsonString = ''
+    let searchResults = []
+    cseResponse.on('data', data => {
+      jsonString += data
+    })
+    cseResponse.on('end', () => {
+      let cseResult = JSON.parse(jsonString)
+      let items = cseResult.items
+      items.map(item => {
+        let resultItem = {
+          url: item.link,
+          snippet: item.title,
+          thumbnail: item.image.thumbnailLink,
+          context: item.image.contextLink
+        }
+        searchResults.push(resultItem)
+      })
+      res.end(JSON.stringify(searchResults))
+      // res.end(jsonString)
+    })
+  })
+
+  cseRequest.on('error', e => {
+    console.log(e)
+  })
+
+  cseRequest.end()
 })
 
 app.listen(port, () => {
